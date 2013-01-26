a188f382abcf402710b63b55d9857916ff2fb7c8
Nathan Edwards <npfedwards@gmail.com>
2013-01-26 15:53:10 +0000

    User list links

--- application/views/user_home.php
+++ application/views/user_home.php
@@ -7,8 +7,12 @@
 ?>
     <h3>Hi <?php echo html_escape($email);?></h3>
     <h4>Here's your links</h4>
+    <ul>
     <?php
-        foreach($links->result() as $link){
-            echo $link->name;
+        if($links !== false){
+            foreach($links->result() as $link){
+                echo "<li><a href='/".$link->name."'>picopath.com/".$link->name."</a> - <a href='".$link->url."'>".$link->url."</a></li>";
+            }
         }
     ?>
+    </ul>
